abf9b2300ce8d6c130de31c3859b162d16a23286
Louise <louise.cch@gmail.com>
2026-03-04 16:41:17 +0000

    update fromMe filter

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -7,128 +7,134 @@ const {
   getPlatform,
   bot,
   lang,
-} = require('../lib/')
+} = require("../lib/");
 bot(
   {
-    pattern: 'help ?(.*)',
+    pattern: "help ?(.*)",
+    fromMe: false,
     dontAddCommandList: true,
   },
   async (message, match, ctx) => {
     const sorted = ctx.commands
       .slice()
-      .sort((a, b) => (a.name && b.name ? a.name.localeCompare(b.name) : 0))
+      .sort((a, b) => (a.name && b.name ? a.name.localeCompare(b.name) : 0));
 
-    const [date, time] = getDate()
+    const [date, time] = getDate();
 
     const CMD_HELP = [
       lang.plugins.menu.help.format(
         ctx.PREFIX,
         message.pushName,
         time,
-        date.toLocaleString('en', { weekday: 'long' }),
-        date.toLocaleDateString('hi'),
+        date.toLocaleString("en", { weekday: "long" }),
+        date.toLocaleDateString("hi"),
         ctx.VERSION,
         ctx.pluginsCount,
         getRam(),
-        getUptime('t'),
-        getPlatform()
+        getUptime("t"),
+        getPlatform(),
       ),
-      '╭────────────────',
-    ]
+      "╭────────────────",
+    ];
 
     sorted.forEach((command, i) => {
       if (!command.dontAddCommandList && command.pattern !== undefined) {
         CMD_HELP.push(
           `│ ${i + 1} ${addSpace(i + 1, sorted.length)}${textToStylist(
             command.name.toUpperCase(),
-            'mono'
-          )}`
-        )
+            "mono",
+          )}`,
+        );
       }
-    })
+    });
 
-    CMD_HELP.push('╰────────────────')
+    CMD_HELP.push("╰────────────────");
 
-    return await message.send(CMD_HELP.join('\n'))
-  }
-)
+    return await message.send(CMD_HELP.join("\n"));
+  },
+);
 
 bot(
   {
-    pattern: 'list ?(.*)',
+    pattern: "list ?(.*)",
     dontAddCommandList: true,
   },
   async (message, match, ctx) => {
     const sorted = ctx.commands
       .slice()
-      .sort((a, b) => (a.name && b.name ? a.name.localeCompare(b.name) : 0))
+      .sort((a, b) => (a.name && b.name ? a.name.localeCompare(b.name) : 0));
 
     const commandList = sorted
-      .filter((command) => !command.dontAddCommandList && command.pattern !== undefined)
+      .filter(
+        (command) =>
+          !command.dontAddCommandList && command.pattern !== undefined,
+      )
       .map((command) => `- *${command.name}*\n${command.desc}\n`)
-      .join('\n')
+      .join("\n");
 
-    await message.send(commandList)
-  }
-)
+    await message.send(commandList);
+  },
+);
 bot(
   {
-    pattern: 'menu ?(.*)',
+    pattern: "menu ?(.*)",
     dontAddCommandList: true,
   },
   async (message, match, ctx) => {
-    const commands = {}
+    const commands = {};
 
     ctx.commands.forEach((command) => {
       if (!command.dontAddCommandList && command.pattern !== undefined) {
-        let cmdType = command.type.toLowerCase()
-        if (!commands[cmdType]) commands[cmdType] = []
+        let cmdType = command.type.toLowerCase();
+        if (!commands[cmdType]) commands[cmdType] = [];
 
-        let isDisabled = command.active === false
-        let cmd = command.name.trim()
-        commands[cmdType].push(isDisabled ? `${cmd} [${lang.plugins.menu.disabled}]` : cmd)
+        let isDisabled = command.active === false;
+        let cmd = command.name.trim();
+        commands[cmdType].push(
+          isDisabled ? `${cmd} [${lang.plugins.menu.disabled}]` : cmd,
+        );
       }
-    })
+    });
 
-    const sortedCommandKeys = Object.keys(commands).sort()
+    const sortedCommandKeys = Object.keys(commands).sort();
 
-    const [date, time] = getDate()
+    const [date, time] = getDate();
     let msg = lang.plugins.menu.menu.format(
       ctx.PREFIX,
       message.pushName,
       time,
-      date.toLocaleString('en', { weekday: 'long' }),
-      date.toLocaleDateString('hi'),
+      date.toLocaleString("en", { weekday: "long" }),
+      date.toLocaleDateString("hi"),
       ctx.VERSION,
       ctx.pluginsCount,
       getRam(),
-      getUptime('t'),
-      getPlatform()
-    )
+      getUptime("t"),
+      getPlatform(),
+    );
 
-    msg += '\n'
+    msg += "\n";
 
     if (match && commands[match]) {
-      msg += ` ╭─❏ ${textToStylist(match.toLowerCase(), 'smallcaps')} ❏\n`
+      msg += ` ╭─❏ ${textToStylist(match.toLowerCase(), "smallcaps")} ❏\n`;
       commands[match]
         .sort((a, b) => a.localeCompare(b))
         .forEach((plugin) => {
-          msg += ` │ ${textToStylist(plugin.toUpperCase(), 'mono')}\n`
-        })
-      msg += ` ╰─────────────────`
-      return await message.send(msg)
+          msg += ` │ ${textToStylist(plugin.toUpperCase(), "mono")}\n`;
+        });
+      msg += ` ╰─────────────────`;
+      return await message.send(msg);
     }
 
     for (const command of sortedCommandKeys) {
-      msg += ` ╭─❏ ${textToStylist(command.toLowerCase(), 'smallcaps')} ❏\n`
+      msg += ` ╭─❏ ${textToStylist(command.toLowerCase(), "smallcaps")} ❏\n`;
       commands[command]
         .sort((a, b) => a.localeCompare(b))
         .forEach((plugin) => {
-          msg += ` │ ${textToStylist(plugin.toUpperCase(), 'mono')}\n`
-        })
-      msg += ` ╰─────────────────\n`
+          msg += ` │ ${textToStylist(plugin.toUpperCase(), "mono")}\n`;
+        });
+      msg += ` ╰─────────────────\n`;
     }
 
-    await message.send(msg.trim())
-  }
-)
+    await message.send(msg.trim());
+  },
+);
